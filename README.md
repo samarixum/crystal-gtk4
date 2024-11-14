6e3c54aab71caf6f4314e87868b985815ada7ddd
Nill≠Null <66809702+samarixum@users.noreply.github.com>
2024-11-14 21:52:11 +1100

    new file:   .editorconfig 	new file:   .gitignore 	new file:   README.md 	new file:   shard.lock 	new file:   shard.yml 	new file:   spec/crystal-gtk4_spec.cr 	new file:   spec/spec_helper.cr 	new file:   src/crystal-gtk4.cr

---
+++ README.md
@@ -0,0 +1,14 @@
+# crystal-gtk4
+
+
+## Build
+
+> shards install
+
+> bin/gi-crystal
+
+> shards build
+
+> ./bin/crystal-gtk4
+
+
---
+++ src/crystal-gtk4.cr
@@ -0,0 +1,20 @@
+require "gtk4"
+
+app = Gtk::Application.new("hello.example.com", Gio::ApplicationFlags::None)
+count = 0
+
+app.activate_signal.connect do
+    window = Gtk::ApplicationWindow.new(app)
+    window.title = "Hello World!"
+    window.set_default_size(200, 200)
+
+    button = Gtk::Button.new_with_label("Hello!!")
+    button.clicked_signal.connect do
+        count += 1
+        button.label = "You clicked #{count} times!"
+    end
+    window.child = button
+    window.present
+end
+
+exit(app.run)
